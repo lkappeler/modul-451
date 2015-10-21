d08aab699f76aabdca8c13f66a6d2515ed7cb89c
Lorenzo Kappeler <lorenzo.kappeler@netcase.ch>
2015-10-21 21:49:35 +0200

    Uint Test for Person model

--- public/local.dev/Tests/PersonTest.php
+++ public/local.dev/Tests/PersonTest.php
@@ -2,13 +2,34 @@
 
 namespace LNK\Tests;
 
-class PersonTest extends PHPUnit_Framework_TestCase {
+require_once __DIR__.DIRECTORY_SEPARATOR.'../Classes/Person.php';
+
+class PersonTest extends \PHPUnit_Framework_TestCase {
 
 	public function testEmpty() {
 		$stack = array();
 		$this->assertEmpty($stack);
 
 		return $stack;
+	}
+
+	public function testPerson() {
+
+		$person = new \LNK\Classes\Person('Hans', 'Muster', '01-01-1970');
+
+		$this->assertEquals('Hans', $person->getFirstName());
+		$this->assertEquals('Muster', $person->getLastName());
+		$this->assertEquals('01-01-1970', $person->getDateOfBirth());
+
+		$person->setFirstName('Fidel');
+		$person->setLastName('Castro');
+		$person->setDateOfBirth(new \DateTime(13-06-1926));
+
+		$this->assertEquals('Fidel', $person->getFirstName());
+		$this->assertEquals('Castro', $person->getLastName());
+		$this->assertEquals(new \DateTime(13-06-1926), $person->getDateOfBirth());
+
+		return $person;
 	}
 
 }
