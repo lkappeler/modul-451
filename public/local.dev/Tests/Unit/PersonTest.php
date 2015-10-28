68528e4aac42b278b156f53b30b7e1fe4792e489
Lorenzo Kappeler <lorenzo.kappeler@netcase.ch>
2015-10-28 18:04:15 +0100

    Remove fail Test

--- public/local.dev/Tests/Unit/PersonTest.php
+++ public/local.dev/Tests/Unit/PersonTest.php
@@ -2,7 +2,7 @@
 
 namespace LNK\Tests;
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '../Classes/Person.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../../Classes/Person.php';
 
 class PersonTest extends \PHPUnit_Framework_TestCase {
 
@@ -38,10 +38,4 @@ class PersonTest extends \PHPUnit_Framework_TestCase {
 
 		return $person;
 	}
-
-	public function failTest() {
-
-		return FALSE;
-	}
-
 }
